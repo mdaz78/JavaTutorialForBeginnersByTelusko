ad306be792e3630811597436b3dde3d9a1c8d91c
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-16 12:28:37 +0530

    update folder structure

---
+++ src/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+  public static void main(String args[]) {
+    // primitive data types
+    int i = 5;
+    byte b = 7;
+    short sh = 77;
+    long l = 20202l;
+
+    float f = 66.6f;
+    double d = 0.09;
+
+    // print
+    System.out.println(i);
+    System.out.println(b);
+    System.out.println(sh);
+    System.out.println(l);
+    System.out.println(f);
+    System.out.println(d);
+
+  }
+}
