3a438aecab1dc50831983e20ef4e3c5f927c166e
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-19 15:10:20 +0530

    lecture35 - Mutable vs Immutable Strings in Java

--- src/LearningStrings.java
+++ src/LearningStrings.java
@@ -6,5 +6,15 @@ public class LearningStrings {
     System.out.println(name.concat(" Mustafa"));
     System.out.println(name.hashCode());
     System.out.println(name.charAt(2));
+
+    System.out.println("--------");
+
+    // Mutable vs Immutable Strings
+    // Strings are by default Mutable
+    String name1 = "Zafar";
+    System.out.println(name.equals(name1));
+
+    name += " Mustafa";
+    System.out.println(name);
   }
 }
