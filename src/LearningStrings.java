2fb1ae8fc6828c89e10537ca28b4a536cd2b9362
mdabuzafar <mdabuzafar78@gmail.com>
2025-05-19 14:57:02 +0530

    lecture34 - What is Strings in Java

---
+++ src/LearningStrings.java
@@ -0,0 +1,10 @@
+public class LearningStrings {
+  public static void main(String[] args) {
+    // Strings Basics
+    String name = "Zafar";
+
+    System.out.println(name.concat(" Mustafa"));
+    System.out.println(name.hashCode());
+    System.out.println(name.charAt(2));
+  }
+}
